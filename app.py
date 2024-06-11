ffbbbfb1f7e6905b0744ef5da961c4a58796683e
Vitor Miranda <vmaquino77@gmail.com>
2024-06-11 20:21:16 -0300

    inclusao de senha

--- app.py
+++ app.py
@@ -14,58 +14,61 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# Título principal
-st.title("🌹 Minha Cartinha 🌹")
+senha = st.text_input('INSIRA SUA SENHA:')
 
-# Seção de boas-vindas
-st.header("Para minha Ursinha!")
-st.write("""
-Em tão pouco tempo, já temos tantas lembranças inesquecíveis e um que amor que não cabe meu coração!!!!
-Você me faz muito feliz e espero que eu possa fazer o mesmo. Em cada momento, como um cineminha,
-um restaurante diferente que visitamos, nos nossos treinos ou em nossos momentos juntinhos. 
-Seja no frio ou no calor, eu tenho uma certeza que é que eu te amo mais !!!!❤️
-""")
+if senha == "tortinhadelimao":
+    # Título principal
+    st.title("🌹 Minha Cartinha 🌹")
 
-# Linha do Tempo do Relacionamento
-st.header("Nossa Linha do Tempo")
-st.subheader("6 momentos especiais para celebrar os nossos 6 meses:")
+    # Seção de boas-vindas
+    st.header("Para minha Ursinha!")
+    st.write("""
+    Em tão pouco tempo, já temos tantas lembranças inesquecíveis e um que amor que não cabe meu coração!!!!
+    Você me faz muito feliz e espero que eu possa fazer o mesmo. Em cada momento, como um cineminha,
+    um restaurante diferente que visitamos, nos nossos treinos ou em nossos momentos juntinhos. 
+    Seja no frio ou no calor, eu tenho uma certeza que é que eu te amo mais !!!!❤️
+    """)
 
-# Adicione fotos e descrições dos marcos
-with st.expander("Esse momento marcou a primeira vez que eu conheci os seus pais"):
-    # Colação Bruna
-    display_image("images/colacao-bruna-1.jpeg", "Colação de grau da melhor Cirurgiã Dentista do Mundo")
+    # Linha do Tempo do Relacionamento
+    st.header("Nossa Linha do Tempo")
+    st.subheader("6 momentos especiais para celebrar os nossos 6 meses:")
 
-with st.expander("Esse momento poderia não ter acontecido, mas ainda bem que você é insistente"):
-    # FORMATURA BRUNA
-    display_image("images/formatura-bruna-1.jpeg", "Formatura Top em Marília")
+    # Adicione fotos e descrições dos marcos
+    with st.expander("Esse momento marcou a primeira vez que eu conheci os seus pais"):
+        # Colação Bruna
+        display_image("images/colacao-bruna-1.jpeg", "Colação de grau da melhor Cirurgiã Dentista do Mundo")
 
-with st.expander("Outro evento que quase não aconteceu, mas foi BA-BA-DO KKKKKK"):
-    # FORMATURA BRUNA
-    display_image("images/ano-novo-1.jpeg", "Ano Novo em Américo")
+    with st.expander("Esse momento poderia não ter acontecido, mas ainda bem que você é insistente"):
+        # FORMATURA BRUNA
+        display_image("images/formatura-bruna-1.jpeg", "Formatura Top em Marília")
 
-with st.expander("Sua primeira vez me visitando em São Paulo e conhecendo minha família"):
-    # FORMATURA BRUNA
-    display_image("images/pinacoteca-1.jpeg", "Turistando pela Pinacoteca")
+    with st.expander("Outro evento que quase não aconteceu, mas foi BA-BA-DO KKKKKK"):
+        # FORMATURA BRUNA
+        display_image("images/ano-novo-1.jpeg", "Ano Novo em Américo")
 
-with st.expander("Formatura ou Disputa por Espaço? Nessa valsa o que mais rolou foi trombada"):
-    # FORMATURA BRUNA
-    display_image("images/formatura-vitor-1.jpeg", "Minha Formatura cheia de familiares")
+    with st.expander("Sua primeira vez me visitando em São Paulo e conhecendo minha família"):
+        # FORMATURA BRUNA
+        display_image("images/pinacoteca-1.jpeg", "Turistando pela Pinacoteca")
 
-with st.expander("Conhecendo a Famosaaaa Tia Cidinha"):
-    # FORMATURA BRUNA
-    display_image("images/aniv-tio-1.jpeg", "Aniversário do seu Tio Avô")    
+    with st.expander("Formatura ou Disputa por Espaço? Nessa valsa o que mais rolou foi trombada"):
+        # FORMATURA BRUNA
+        display_image("images/formatura-vitor-1.jpeg", "Minha Formatura cheia de familiares")
 
-
-# Galeria de Fotos
-st.header("Galeria de Fotos")
-
-# Adicione fotos à galeria
-with st.expander("Algumas fotinhas extras de nós dois:"):
-    display_image("images/restaurante-1.jpeg", "Nós fazendo o que mais gostamos de fazer")
-    display_image("images/formatura-bruna-2.jpeg", "Roda Gigante que te assustou muito")
-    display_image("images/formatura-bruna-3.jpeg", "Nós dancançando muito na sua valsa")
-    display_image("images/ano-novo-2.jpeg", "Dois fofos")
+    with st.expander("Conhecendo a Famosaaaa Tia Cidinha"):
+        # FORMATURA BRUNA
+        display_image("images/aniv-tio-1.jpeg", "Aniversário do seu Tio Avô")    
 
 
-# Rodapé
-st.write("Feito com ❤️ por Vitor Miranda")
+    # Galeria de Fotos
+    st.header("Galeria de Fotos")
+
+    # Adicione fotos à galeria
+    with st.expander("Algumas fotinhas extras de nós dois:"):
+        display_image("images/restaurante-1.jpeg", "Nós fazendo o que mais gostamos de fazer")
+        display_image("images/formatura-bruna-2.jpeg", "Roda Gigante que te assustou muito")
+        display_image("images/formatura-bruna-3.jpeg", "Nós dancançando muito na sua valsa")
+        display_image("images/ano-novo-2.jpeg", "Dois fofos")
+
+
+    # Rodapé
+    st.write("Feito com ❤️ por Vitor Miranda")
